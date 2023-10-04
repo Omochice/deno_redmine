95a62aaa1dca754850e126d7ee74393ca5d0b743
Omochice <mochice.mls.ntl@gmail.com>
2023-10-04 19:09:54 +0900

    feat: add update function

---
+++ projects/update.ts
@@ -0,0 +1,45 @@
+import { err, ok, Result } from "npm:neverthrow@6.0.0";
+import { join } from "https://deno.land/std@0.197.0/path/mod.ts";
+import type { Context } from "../context.ts";
+import type { ProjectRequest } from "./type.ts";
+import {
+  array,
+  is,
+  object,
+  string,
+} from "https://deno.land/x/valibot@v0.18.0/mod.ts";
+
+const errorSchema = object({
+  errors: array(string()),
+});
+
+export type ProjectUpdateInformation = Partial<
+  Omit<ProjectRequest, "identifier">
+>;
+
+export async function update(
+  id: number,
+  project: ProjectUpdateInformation,
+  context: Context,
+): Promise<Result<void, Error>> {
+  const url = new URL(join(context.endpoint, "projects", `${id}.json`));
+  const response = await fetch(
+    url,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Redmine-API-Key": context.apiKey,
+      },
+      body: JSON.stringify({ project }),
+    },
+  );
+  if (!response.ok) {
+    const json = await response.json();
+    if (!is(errorSchema, json)) {
+      return err(new Error(`${response.status}: ${response.statusText}`));
+    }
+    return err(new Error(JSON.stringify(json.errors)));
+  }
+  return ok(undefined);
+}
