624089f2c1e0246f808ffcecdc8bcb20c3c7e8bb
Omochice <44566328+Omochice@users.noreply.github.com>
2023-10-05 14:11:05 +0900

    feat: add function to update project (#10)
    * refactor: move project type into type.ts
    
    * feat: add update function

--- projects/type.ts
+++ projects/type.ts
@@ -1,9 +1,13 @@
 import {
+  array,
   boolean,
   nullish,
   number,
   object,
+  optional,
   type Output,
+  record,
+  regex,
   string,
   transform,
 } from "https://deno.land/x/valibot@v0.18.0/mod.ts";
@@ -40,3 +44,22 @@ export const projectSchema = transform(inputProjectSchema, (input) => {
 });
 
 export type Project = Output<typeof projectSchema>;
+
+export const projectRequestSchema = object({
+  name: string([regex(/[a-zA-Z0-9\-_]{1,100}/)]),
+  /** need match /[a-zA-Z0-9\-_]{1,100} */
+  identifier: string(),
+  description: optional(string()),
+  homepage: optional(string()),
+  is_public: optional(boolean()),
+  parent_id: optional(number()),
+  inherit_members: optional(boolean()),
+  default_assigned_to_id: optional(number()),
+  default_version_id: optional(string()),
+  tracker_ids: optional(array(number())),
+  enable_module_names: optional(array(string())),
+  issue_custom_field_ids: optional(array(string())),
+  custom_field_values: optional(record(string())),
+});
+
+export type ProjectRequest = Output<typeof projectRequestSchema>;
