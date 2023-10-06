b9b30cb77571049fcad8ac70f1fa8b09e2c4e3c7
Omochice <mochice.mls.ntl@gmail.com>
2023-10-06 15:56:25 +0900

    feat: add function to archive/unarvhice project

---
+++ projects/archive.ts
@@ -0,0 +1,55 @@
+import {
+  array,
+  is,
+  object,
+  string,
+} from "https://deno.land/x/valibot@v0.18.0/mod.ts";
+import { err, ok, Result } from "npm:neverthrow@6.0.0";
+import { join } from "https://deno.land/std@0.203.0/path/mod.ts";
+import type { Context } from "../context.ts";
+
+const errorSchema = object({
+  errors: array(string()),
+});
+
+async function internal(
+  id: number,
+  method: "archive" | "unarchive",
+  context: Context,
+): Promise<Result<void, Error>> {
+  const url = new URL(
+    join(context.endpoint, "projects", `${id}`, `${method}.json`),
+  );
+  const response = await fetch(
+    url,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Redmine-API-Key": context.apiKey,
+      },
+    },
+  );
+  if (!response.ok) {
+    const json = await response.json();
+    if (!is(errorSchema, json)) {
+      return err(new Error(`${response.status}: ${response.statusText}`));
+    }
+    return err(new Error(JSON.stringify(json.errors)));
+  }
+  return ok(undefined);
+}
+
+export async function archive(
+  id: number,
+  context: Context,
+): Promise<Result<void, Error>> {
+  return await internal(id, "archive", context);
+}
+
+export async function unarchive(
+  id: number,
+  context: Context,
+): Promise<Result<void, Error>> {
+  return await internal(id, "unarchive", context);
+}
