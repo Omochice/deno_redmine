9916d568d7cf9c49d953cd91cede418b39d1a397
Omochice <mochice.mls.ntl@gmail.com>
2023-11-08 13:57:50 +0900

    refactor: use ResultAsync instead (projects/delete)

--- projects/delete.ts
+++ projects/delete.ts
@@ -4,35 +4,47 @@ import {
   object,
   string,
 } from "https://deno.land/x/valibot@v0.18.0/mod.ts";
-import { err, ok, Result } from "npm:neverthrow@6.1.0";
+import { errAsync, okAsync, ResultAsync } from "npm:neverthrow@6.1.0";
 import { join } from "https://deno.land/std@0.205.0/path/mod.ts";
 import type { Context } from "../context.ts";
+import { convertError } from "../error.ts";
 
 const errorSchema = object({
   errors: array(string()),
 });
 
-export async function deleteProject(
+export function deleteProject(
   id: number,
   context: Context,
-): Promise<Result<void, Error>> {
+): ResultAsync<void, Error> {
   const url = new URL(join(context.endpoint, "projects", `${id}.json`));
-  const response = await fetch(
-    url,
-    {
+  return ResultAsync.fromPromise(
+    fetch(url, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         "X-Redmine-API-Key": context.apiKey,
       },
-    },
-  );
-  if (!response.ok) {
-    const json = await response.json();
-    if (!is(errorSchema, json)) {
-      return err(new Error(`${response.status}: ${response.statusText}`));
-    }
-    return err(new Error(JSON.stringify(json.errors)));
-  }
-  return ok(undefined);
+    }),
+    convertError("Unexpected Error"),
+  )
+    .andThen((r: Response) => r.ok ? okAsync(undefined) : errAsync(r))
+    .orElse((e: Response | Error) => {
+      if (e instanceof Error) {
+        return errAsync(e);
+      }
+      return ResultAsync.fromPromise(
+        e.json(),
+        convertError("Unexpected Error"),
+      );
+    })
+    .andThen((r: unknown) => {
+      if (r === undefined) {
+        return okAsync(undefined);
+      }
+      if (is(errorSchema, r)) {
+        return errAsync(new Error(JSON.stringify(r.errors)));
+      }
+      return errAsync(new Error("Unexpected Error", { cause: r }));
+    });
 }
