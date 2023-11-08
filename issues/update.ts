a1a113ca8740a33d42a6454898f6fe1cc9c4f85d
Omochice <mochice.mls.ntl@gmail.com>
2023-11-08 19:35:03 +0900

    refactor: use ResultAsync instead

--- issues/update.ts
+++ issues/update.ts
@@ -1,4 +1,4 @@
-import { err, ok, Result } from "npm:neverthrow@6.1.0";
+import { errAsync, okAsync, ResultAsync } from "npm:neverthrow@6.1.0";
 import { Context } from "../context.ts";
 import { Issue } from "./type.ts";
 import { join } from "https://deno.land/std@0.205.0/path/mod.ts";
@@ -8,6 +8,7 @@ import {
   object,
   string,
 } from "https://deno.land/x/valibot@v0.18.0/mod.ts";
+import { convertError } from "../error.ts";
 
 type UpdateOption = {
   notes?: string;
@@ -18,29 +19,40 @@ const errorSchema = object({
   errors: array(string()),
 });
 
-export async function update(
+export function update(
   id: number,
   issue: Partial<Issue & UpdateOption>,
   context: Context,
-): Promise<Result<void, Error>> {
+): ResultAsync<void, Error> {
   const url = new URL(join(context.endpoint, "issues", `${id}.json`));
-  const response = await fetch(
-    url,
-    {
+  return ResultAsync.fromPromise(
+    fetch(url, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "X-Redmine-API-Key": context.apiKey,
       },
       body: JSON.stringify({ issue }),
-    },
-  );
-  if (!response.ok) {
-    const json = await response.json();
-    if (!is(errorSchema, json)) {
-      return err(new Error(`${response.status}: ${response.statusText}`));
-    }
-    return err(new Error(JSON.stringify(json.errors)));
-  }
-  return ok(undefined);
+    }),
+    convertError("Unexpected Error"),
+  )
+    .andThen((r: Response) => r.ok ? okAsync(undefined) : errAsync(r))
+    .orElse((e: Response | Error) => {
+      if (e instanceof Error) {
+        return errAsync(e);
+      }
+      return ResultAsync.fromPromise(
+        e.json(),
+        convertError("Unexpected Error"),
+      );
+    })
+    .andThen((r: unknown) => {
+      if (r === undefined) {
+        return okAsync(undefined);
+      }
+      if (is(errorSchema, r)) {
+        return errAsync(new Error(JSON.stringify(r.errors)));
+      }
+      return errAsync(new Error("Unexpected Error", { cause: r }));
+    });
 }
