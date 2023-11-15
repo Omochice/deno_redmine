699a3639f2649d581d6a0a13b84c1b2d54a481fb
Omochice <mochice.mls.ntl@gmail.com>
2023-11-15 11:07:43 +0900

    feat: add update time entry

---
+++ timeEntries/update.ts
@@ -0,0 +1,32 @@
+import { errAsync, okAsync, ResultAsync } from "npm:neverthrow@6.1.0";
+import { parse } from "https://deno.land/x/valibot@v0.18.0/mod.ts";
+import { join } from "https://deno.land/std@0.205.0/url/join.ts";
+import type { Context } from "../context.ts";
+import { UploadTimeEntry, uploadTimeEntrySchema } from "./type.ts";
+import { convertError } from "../error.ts";
+
+export function update(
+  id: number,
+  timeEntry: UploadTimeEntry,
+  context: Context,
+): ResultAsync<undefined, Error> {
+  const entry = parse(uploadTimeEntrySchema, timeEntry);
+  return ResultAsync.fromPromise(
+    fetch(join(context.endpoint, "time_entries", `${id}.json`), {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Redmine-API-Key": context.apiKey,
+      },
+      body: JSON.stringify({ time_entry: entry }),
+    }),
+    convertError("Unexpected Error"),
+  )
+    .andThen((r: Response) => r.ok ? okAsync(undefined) : errAsync(r))
+    .orElse((e: Response | Error) => {
+      if (e instanceof Error) {
+        return errAsync(e);
+      }
+      return errAsync(new Error(`Error: ${e.status}`, { cause: e }));
+    });
+}
