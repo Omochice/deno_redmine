d430f42537c0a48e6a45b31ad0d13eb81be8ac11
Omochice <mochice.mls.ntl@gmail.com>
2024-06-24 02:06:57 +0900

    chore: replace deno.land/x to jsr for valibot

--- issues/type.ts
+++ issues/type.ts
@@ -11,7 +11,7 @@ import {
   string,
   transform,
   union,
-} from "https://deno.land/x/valibot@v0.30.0/mod.ts";
+} from "jsr:@valibot/valibot@0.30.0";
 
 export type IdName = Output<typeof IdName>;
 export const IdName = object({
--- projects/update.ts
+++ projects/update.ts
@@ -2,12 +2,7 @@ import { errAsync, okAsync, ResultAsync } from "npm:neverthrow@6.2.2";
 import { join } from "jsr:@std/path@0.225.2";
 import type { Context } from "../context.ts";
 import type { ProjectRequest } from "./type.ts";
-import {
-  array,
-  is,
-  object,
-  string,
-} from "https://deno.land/x/valibot@v0.30.0/mod.ts";
+import { array, is, object, string } from "jsr:@valibot/valibot@0.30.0";
 import { convertError } from "../error.ts";
 
 const errorSchema = object({
