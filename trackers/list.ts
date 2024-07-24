dd7facc8e25a86c734d74afaff2a301c7f8a3ac1
Omochice <44566328+Omochice@users.noreply.github.com>
2024-07-24 19:03:00 +0900

    feat: add tracker (#82)
    * feat: add trackers
    
    * docs: check trackers

---
+++ trackers/list.ts
@@ -0,0 +1,42 @@
+import { array, object, parse } from "jsr:@valibot/valibot@0.30.0";
+import { ResultAsync } from "npm:neverthrow@7.0.0";
+import { join } from "jsr:@std/url@1.0.0-rc.2";
+import { type Tracker, trackerSchema } from "./type.ts";
+import type { Context } from "../context.ts";
+import { convertError } from "../error.ts";
+
+const responseSchema = object({
+  trackers: array(trackerSchema),
+});
+
+/**
+ * Fetch trackers
+ *
+ * @param context REST endpoint context
+ * @return Promise of result-type which has tracker or error
+ */
+export const fetchList = ResultAsync.fromThrowable(
+  throwableFetchList,
+  convertError("Unexpected error"),
+);
+
+/**
+ * Fetch trackers
+ *
+ * @param context REST endpoint context
+ * @return Array of Tracker
+ */
+async function throwableFetchList(context: Context): Promise<Tracker[]> {
+  const endpoint = join(context.endpoint, "trackers.json");
+  const response = await fetch(
+    endpoint,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Redmine-API-Key": context.apiKey,
+      },
+    },
+  );
+  return parse(responseSchema, await response.json()).trackers;
+}
