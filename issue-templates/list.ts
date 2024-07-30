1b80fb7ae910b376364c23898711abe69bf9a953
Omochice <44566328+Omochice@users.noreply.github.com>
2024-07-31 02:21:22 +0900

    feat: add `issue-template` (#104)
    * feat: add fetch list of issue templates
    
    * feat: add client of issue-template
    
    * chore: update version of valibot
    
    * docs: add to list

---
+++ issue-templates/list.ts
@@ -0,0 +1,44 @@
+import type { Context } from "../context.ts";
+import { parse } from "jsr:@valibot/valibot@0.37.0";
+import { ResultAsync } from "npm:neverthrow@7.0.0";
+import { join } from "jsr:@std/url@1.0.0-rc.2";
+import { convertError } from "../error.ts";
+import { type Response_, responseSchema } from "./type.ts";
+
+/**
+ * Fetch list of issue templates
+ * @params context Connection context object
+ * @params projectId Project id or Project identifier
+ */
+async function throwableList(
+  context: Context,
+  projectId: number | string,
+): Promise<Response_> {
+  const endpoint = join(
+    context.endpoint,
+    "projects",
+    `${projectId}`,
+    "issue_templates.json",
+  );
+  const r = await fetch(
+    endpoint,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Redmine-API-Key": context.apiKey,
+      },
+    },
+  );
+  return parse(responseSchema, await r.json());
+}
+
+/**
+ * Fetch list of issue templates
+ * @params context Connection context object
+ * @params projectId Project id or Project identifier
+ */
+export const list = ResultAsync.fromThrowable(
+  throwableList,
+  convertError("Unexpected Error"),
+);
