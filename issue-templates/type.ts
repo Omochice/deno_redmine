3e11c4c62979ccf7e6c85df826bffdd0fba0f9d8
Omochice <mochice.mls.ntl@gmail.com>
2024-07-30 20:32:48 +0900

    feat: add fetch list of issue templates

---
+++ issue-templates/type.ts
@@ -0,0 +1,44 @@
+import {
+  array,
+  boolean,
+  custom,
+  type InferOutput,
+  number,
+  object,
+  pipe,
+  string,
+  transform,
+} from "jsr:@valibot/valibot@0.36.0";
+
+const dateLikeString = pipe(
+  string(),
+  custom((input: unknown) => {
+    if (typeof input !== "string") {
+      return false;
+    }
+    return !isNaN(Date.parse(input));
+  }),
+  transform((input) => new Date(input)),
+);
+
+const templateSchema = object({
+  id: number(),
+  tracker_id: number(),
+  tracker_name: string(),
+  title: string(),
+  issue_title: string(),
+  description: string(),
+  enabled: boolean(),
+  updated_on: dateLikeString,
+  created_on: dateLikeString,
+});
+
+export type IssueTemplate = InferOutput<typeof templateSchema>;
+
+export const responseSchema = object({
+  global_issue_templates: array(templateSchema),
+  inherit_templates: array(templateSchema),
+  issue_templates: array(templateSchema),
+});
+
+export type Response_ = InferOutput<typeof responseSchema>;
